2d1b93ad8acafd99e70d128ddd53953d07cf2d31
Anshuman <chaoanxuman999@gmail.com>
2025-03-13 15:50:20 +0530

    Backend for Valorant_Accessories project

---
+++ src/main/java/com/valorant_accesories/Valorant_Accessories/ValorantAccessoriesApplication.java
@@ -0,0 +1,13 @@
+package com.valorant_accesories.Valorant_Accessories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantAccessoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantAccessoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valorant_accesories/Valorant_Accessories/ValorantAccessoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorant_accesories.Valorant_Accessories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantAccessoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
