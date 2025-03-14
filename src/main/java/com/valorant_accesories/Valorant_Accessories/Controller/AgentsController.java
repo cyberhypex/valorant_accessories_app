a289ffe69fcf6262934fd06856de0c7efeae5a0c
Anshuman <chaoanxuman999@gmail.com>
2025-03-14 14:37:25 +0530

    Added file to fix CORS error

--- src/main/java/com/valorant_accesories/Valorant_Accessories/Controller/AgentsController.java
+++ src/main/java/com/valorant_accesories/Valorant_Accessories/Controller/AgentsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin()
+
 
 @RestController
 @RequestMapping("/valorant")
